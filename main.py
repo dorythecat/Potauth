e32af9c9a40f671650cf9e223afbf44a6f774084
Dory <pemegomachado@gmail.com>
2025-08-06 11:42:07 +0200

    Basic website

---
+++ main.py
@@ -0,0 +1,76 @@
+import jwt
+
+from fastapi import Depends, FastAPI, HTTPException, Security, Body
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jwt import InvalidTokenError
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="Potauth API",
+    summary="The Potato-based authentication API",
+    description="""
+        The Potauth API allows users to authenticate themselves by using potatoes, in a variety of forms, including:
+        - Fried potatoes
+        - Baked potatoes
+        - Stuffed potatoes
+        - Mashed potatoes
+        - And much more!
+        
+        Note: Potauth does not support sweet potatoes.
+    """,
+    version="1.0.0",
+    license_info={
+        "name": "GNU Affero General Public License v3.0 or later",
+        "identifier": "AGPL-3.0-or-later"
+    }
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DATABASE_PATH = "potauth.db" # TODO
+
+ALGORITHM = "HS256" # Algorith for JWT to use to encode tokens
+security = HTTPBearer()
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta if expires_delta else timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_token(credentials: HTTPAuthorizationCredentials = Security(security)) -> str | HTTPException:
+    """Get and validate the Authorization header token."""
+    token = credentials.credentials if credentials else None
+    if not token:
+        return None
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return data['access_token']
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+class ErrorMessage(BaseModel):
+    message: str
+
+@app.get("/login",
+         response_model=None,
+         responses={
+             401: {"model": ErrorMessage},
+             403: {"model": ErrorMessage},
+             404: {"model": ErrorMessage}
+         })
+def login(): # TODO
+    """Login to the API."""
+    return None
