2de134ad9668a46a2bba0d54d1ab42f2f80bca45
Dory <pemegomachado@gmail.com>
2025-08-15 19:33:51 +0200

    Added verification for the secret key

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY: raise Exception("SECRET_KEY not found in .env file.") # Make sure a key is set
 
 app = FastAPI(
     title="Potauth API",
